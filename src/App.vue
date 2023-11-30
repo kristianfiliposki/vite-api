dbb1e87187b9e532ccda9873128bc72006a195bb
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-30 17:11:16 +0100

    i create the component "names"

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,15 @@
 <script >
 
 import axios from 'axios'; /* importo axios */
+import Names from './components/Names.vue';
+
 export default {
-  data() {
-    return {
-      dati: []
-    }
-  },
-  mounted() {
-    this.ServerInfo()
-  },
-  methods: {
-    ServerInfo() {
-      axios.get("https://api.openbrewerydb.org/v1/breweries?by_country=ireland&per_page=10").then(risultato => {
-        this.dati = risultato.data
-        console.log(this.dati)
-      })
-    },
-    info(dati) {
-    }
-  }
+  components: { Names }
 }
 </script>
 
 <template>
-  <div class="box">
-    <div v-for="dato in dati" @click="info()">{{ dato.name }}</div>
-  </div>
+  <Names></Names>
 </template>
 
-<style scoped>
-.box {
-  padding: 30px;
-  border: 1px solid white;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Names.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios';
+export default {
+    name: "Names",
+    data() {
+        return {
+            dati: []
+        };
+    },
+    mounted() {
+        this.ServerInfo();
+    },
+    methods: {
+        ServerInfo() {
+            axios.get("https://api.openbrewerydb.org/v1/breweries?by_country=ireland&per_page=10").then(risultato => {
+                this.dati = risultato.data;
+                console.log(this.dati);
+            });
+        },
+        info(dati) {
+        }
+    },
+}
+</script>
+<template>
+    <div>
+        <div class="box">
+            <h1>Where is yor Pub?</h1>
+            <div class="pub" v-for="dato in dati">{{ dato.name }}</div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.box {
+    padding: 30px;
+    border: 1px solid white;
+}
+
+.pub {
+    margin: 2em;
+    font-size: 1.4em;
+}
+</style>
